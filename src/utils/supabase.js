23c8a6c0d59eb19b662fd7d77e13eb060489d9ca
Sahanilesh <sahaniniles18@gmail.com>
2025-01-07 23:04:27 +0530

    Superbase & Clerk connection

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,9 +1,16 @@
-
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
-        
+const superbaseClient = async (superbaseAccessToken) => {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${superbaseAccessToken}`,
+      },
+    },
+  });
+  return supabase;
+};
+export default superbaseClient;
